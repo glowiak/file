083a4a1ef5e119f28738319eaaf8a5f1d1352320
glowiak <52356948+glowiak@users.noreply.github.com>
2020-04-12 17:21:26 +0200

    bbq4xd
    rgrehtr

---
+++ mclauncher.bat
@@ -0,0 +1,1 @@
+java -Xms256m -Xmx256m -cp "%instances\Forge\bin\minecraft.jar;instances\Forge\bin\jinput.jar;instances\Forge\bin\lwjgl.jar;instances\Forge\bin\lwjgl_util.jar" -Djava.library.path="instances\Forge\bin\natives" net.minecraft.client.Minecraft
