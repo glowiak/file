73c813a6e281d0744d6bfc43ca9a14067ad394e2
glowiak <52356948+glowiak@users.noreply.github.com>
2020-05-24 19:05:17 +0200

    reyhe3h
    t6jy6j

---
+++ randomizer.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import subprocess
+
+def main():
+    while True:
+        os.system("color 0a")
+        os.system("title %random%%random%%random%")
+        os.system("echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%")
+
+
+main()
