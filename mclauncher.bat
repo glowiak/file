9e72bcd05aad70bcea099dd72dbd123fafdfdf51
glowiak <52356948+glowiak@users.noreply.github.com>
2022-04-14 10:38:22 +0200

    Delete mclauncher.bat

--- mclauncher.bat
+++
@@ -1,1 +0,0 @@
-java -Xms256m -Xmx256m -cp "%instances\Forge\bin\minecraft.jar;instances\Forge\bin\jinput.jar;instances\Forge\bin\lwjgl.jar;instances\Forge\bin\lwjgl_util.jar" -Djava.library.path="instances\Forge\bin\natives" net.minecraft.client.Minecraft
